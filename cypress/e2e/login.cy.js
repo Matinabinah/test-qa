cdec4eb31788be2972340f58af29fb6de6d5d68b
Matin <67404191+Matinabinah@users.noreply.github.com>
2025-09-03 21:01:03 -0400

    Matin Abinah QA assigment

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,3 +1,28 @@
 // TODO: Write tests for Lazr login page
-// 1. Test valid login
-// 2. Test invalid login
+
+import LoginPage from '../pages/loginPage';
+
+const loginPage = new LoginPage();
+
+describe('Lazr Login Tests', () => {
+
+    // 1. Valid login test
+    it('Valid Login Flow', () => {
+        loginPage.visitLoginPage();
+        loginPage.enterEmail(Cypress.env("email"));
+        loginPage.clickNext();
+        loginPage.enterPassword(Cypress.env("password"));
+        loginPage.clickSignIn();
+    });
+
+    // 2. Invalid login test
+    it('InValid Login Flow', () => {
+        loginPage.visitLoginPage();
+        loginPage.enterEmail(Cypress.env("email"));
+        loginPage.clickNext();
+        loginPage.enterPassword(Cypress.env("invalidPassword"));
+        loginPage.clickSignIn();
+        loginPage.verifyError();
+    });
+
+});
